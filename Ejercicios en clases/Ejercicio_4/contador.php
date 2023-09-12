9940569f2bc18d80e9979ae1c2363182403d317e
Luis Gustavo Vera Miranda <vera.miranda.luis.gustavo@gmail.com>
2023-09-11 20:06:29 -0400

    commit

---
+++ Ejercicios en clases/Ejercicio_4/contador.php
@@ -0,0 +1,15 @@
+<?php
+
+if(!isset($_COOKIE['contador']))
+{
+    setcookie("contador",1,time()+3600*60);
+    $contador=1;
+}
+else
+{
+    $contador=$_COOKIE['contador'];
+    $contador++;
+    setcookie("contador",$contador,time()+3600*60);
+}
+echo "esta es la $contador ves que visita la pagina";
+?>
---
+++ Ejercicios en clases/Ejercicio_5/conexion.php
@@ -0,0 +1,6 @@
+<?php
+
+$con =new mysqli("localhost","root","","materiales");
+if($con->connect_error){
+    
+}
