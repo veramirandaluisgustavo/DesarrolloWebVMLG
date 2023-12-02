956b1e9740efd24cf61b4616ba4d04b24e317c31
Luis Gustavo Vera Miranda <vera.miranda.luis.gustavo@gmail.com>
2023-12-02 01:29:46 -0400

    final commit maqueta

---
+++ Examenes/Final/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div id="barra_superior">
+            <img src="./images/usfx.png" >
+            <div id="opciones">
+                <div id="menu">
+                    <button class="pregunta-boton">Pregunta 1</button>
+                    <button class="pregunta-boton">Pregunta 2</button>
+                    <button class="pregunta-boton">Pregunta 3</button>
+                    <button class="pregunta-boton">Pregunta 4</button>
+                    <button class="pregunta-boton">Pregunta 5</button>
+                </div>
+                <div id="titulo">
+
+                </div>
+            </div>
+        </div>
+        <div class="pagina">
+            <div id="sub-menu">
+                <button id="boton-opcion">Opciones</button>
+                <button class="opcion">Detalle 1</button>
+                <button class="opcion">Detalle 2</button>
+
+            </div>
+            <div id="contenido">
+                <div class="perfil">
+                    <div id="examen">
+                        <div>SIS 256 Programacion Web</div>
+                        <div>Examen Final - 02-12-2023 7:00 am</div>
+                    </div>
+                    <div id="datos">
+                        <div>VERA MIRANDA LUIS GUSTAVO</div>
+                        <div>Carrera: Ing.Sistemas,Ing.Ciencias de la Computacion</div>
+                        <div>Repositorio:</div>
+                    </div>
+                    <img id="foto" src="./images/foto.png" >
+                </div>
+
+            </div>
+
+        </div>
+        <div id="pie">Sucre – Semestre 2-2023</div>
+    </div>
+    
+</body>
+</html>
